08fc14524c21ce1444e7cea08e03a01d4051800a
MJ_Shelton <mjshelton12@comcast.net>
2022-09-20 18:03:20 -0700

    Updated Interaction and Resolution seeds and models

--- seeds/interaction-seeds.js
+++ seeds/interaction-seeds.js
@@ -4,32 +4,26 @@ const interactionData = [
   {
     location_id: 1,
     interaction: "Go to Kitchen",
-    resolution_id: 2
   },
   {
     location_id: 1,
     interaction: "Go to Bathroom",
-    resolution_id: 3
   },
   {
     location_id: 2,
     interaction: "Go to Bedroom",
-    resolution_id: 1
   },
   {
     location_id: 2,
     interaction: "Go to Bathroom",
-    resolution_id: 3
   },
   {
     location_id: 3,
     interaction: "Go to Bedroom",
-    resolution_id: 1
   },
   {
     location_id: 3,
     interaction: "Go to Kitchen",
-    resolution_id: 2
   }
 ];
 
