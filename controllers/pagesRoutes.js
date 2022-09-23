e99651469323036965eeab1b715531b37c29d7f0
MJ_Shelton <mjshelton12@comcast.net>
2022-09-22 18:49:55 -0700

    Built out start and partials in handlebars

--- controllers/pagesRoutes.js
+++ controllers/pagesRoutes.js
@@ -1,9 +1,32 @@
+const { Character, Location, Inventory, Interaction } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/Start', async (req, res) => {
     try {
+      const initializeCharacter = await Character.findOne({ where: id = 1,
+      include: [
+        {
+          model: Location,
+          include:
+            [{
+                model: Interaction
+              }]
+        }
+        // {
+        //   model: Inventory
+        // }
+      ]
+    });
+
+    console.log("Line 22 " + JSON.stringify(initializeCharacter))
+
+    const character = initializeCharacter.get({ plain: true });
+
+    console.log("Line 27 " + character)
     
         res.render('Start', {
+            ...character,
             loggedIn: req.session.loggedIn,
           });
         } catch (err) {
@@ -11,5 +34,7 @@ router.get('/Start', async (req, res) => {
           res.status(500).json(err);
         }
       });
-      
+
+//Post character in a location
+
 module.exports = router;
