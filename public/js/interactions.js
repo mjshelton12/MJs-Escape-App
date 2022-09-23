48a259dcb167c2b8166341ace66d166204aa3826
MJ_Shelton <mjshelton12@comcast.net>
2022-09-23 12:12:56 -0700

    Built interactions.js and bug fixing routes

---
+++ public/js/interactions.js
@@ -0,0 +1,41 @@
+const interactionList = document.querySelector('.interaction-list')
+
+console.log(interactionList)
+
+const processInteraction = async (btnData) => {
+
+    console.log(btnData.attributes)
+    
+    const location = btnData.getAttribute('location_patch')
+    const itemPost = btnData.getAttribute('item_post')
+
+    if (location) {
+        const response = await fetch('/api/characters/1', {
+            method: 'PATCH',
+            body: JSON.stringify({ "location_id" : location }),
+            headers: { 'Content-Type': 'application/json' },
+          });
+          console.log(response) 
+          if (response.ok) {
+            // document.location.replace('/');
+          } else {
+            alert('Failed to change locations.');
+          }
+        }
+    
+    if (itemPost) {
+
+    }
+
+}
+
+// when you click a button, it takes you to a patch route that, based on the interaction attached to the button, changes the character's location
+
+
+interactionList.onclick = function(event) {
+    let target = event.target;
+  
+    if (target.className!= 'interactions-btn') return;
+
+    processInteraction(target);
+  };
